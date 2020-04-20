c90a5d40b9e7958f5ec0c085dc2bc917d7b32b2a
Nick Chapman <nic@amadido.co.uk>
2020-04-20 22:30:38 +0100

    split Reference.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,14 @@
 module Main(main) where
 
 import Ast
+import Reference(reference)
 --import Eval(evaluate)
 import Eval_Instrumented(evaluate)
 import Norm_Final(normalize)
 
 main :: IO ()
 main = do
-  print (_original 9)
-  print (_goal 9)
+  reference
   demo "original" prog
   let progN = normalize prog
   demo "normalized" progN
@@ -26,21 +26,3 @@ prog = Lam "arg" (
   Let "twice" (Lam "f" (Lam "x" (App (Var "f") (App (Var "f") (Var "x"))))) (
   Let "increase" (Lam "q" (Lam "x" (Add (App (Var "dub") (Var "x")) (Var "q")))) (
   App (App (Var "twice") (App (Var "increase") (Add (Var "arg") (Num 1)))) (Num 3)))))
-
-
-
--- Haskell versions for reference
-
-_original :: Int -> Int
-_original = \arg -> do
-  let dub x = x + x
-  let twice f x = f (f x)
-  let increase q x = dub x + q
-  twice (increase (arg+1)) 3
-
-_goal :: Int -> Int
-_goal = \arg -> do
-  let q = arg + 1
-  let x0 = 3
-  let x1 = (x0 + x0) + q
-  (x1 + x1) + q
---
+++ src/Reference.hs
@@ -0,0 +1,42 @@
+module Reference(reference) where
+
+-- Haskell reference for example program...
+
+reference :: IO ()
+reference = do
+  putStrLn $ "original(9) = " <> show (original 9)
+  putStrLn $ "goal(9) = " <> show (goal 9)
+  putStrLn $ "nearly(9) = " <> show (nearly 9)
+
+
+original :: Int -> Int
+original = \arg -> do
+  let dub x = x + x
+  let twice f x = f (f x)
+  let increase q x = dub x + q
+  twice (increase (arg+1)) 3
+
+
+goal :: Int -> Int
+goal = \arg -> do
+  let q = arg + 1
+  let x0 = 3
+  let x1 = (x0 + x0) + q
+  (x1 + x1) + q
+
+
+
+
+
+
+
+
+
+
+
+
+
+nearly :: Int -> Int
+nearly = \arg -> do
+  let f = let q = arg + 1 in \x -> (x + x) + q
+  f (f 3)
