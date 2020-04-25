d10533b496e162186bd40e1534fe740bd4d9d863
Nick Chapman <nic@amadido.co.uk>
2020-04-25 09:25:57 +0100

    improve counting of Micro steps in original Cek version

--- src/Cek.hs
+++ src/Cek.hs
@@ -17,26 +17,24 @@ data Value
   deriving (Show)
 
 evaluate :: Exp -> Result
-evaluate e = loop counts0 (inject e) where
-  loop :: Counts -> Machine -> Result
-  loop counts m = do
+evaluate e = loop (install e) where
+  loop :: Machine -> Result
+  loop m = do
     case finished m of
-      Just v -> Result v (bump m counts)
-      Nothing -> loop (bump m counts) (step m)
+      Just res -> res
+      Nothing -> loop (step (tick [DoOuterStep] m))
 
-inject :: Exp -> Machine
-inject e = (CE e, Map.empty, Kdone)
+install :: Exp -> Machine
+install e = (counts0, CE e, Map.empty, Kdone)
 
-finished :: Machine -> Maybe Value
-finished = \case (CV v, _, Kdone) -> Just v; _ -> Nothing
+finished :: Machine -> Maybe Result
+finished = \case (i, CV v, _, Kdone) -> Just (Result v i); _ -> Nothing
 
-look :: Var -> Env -> Value
-look x env = maybe (error $ "lookup:" ++ x) id (Map.lookup x env)
-
-type Machine = (Control,Env,Kont)
-data Control = CE Exp | CV Value deriving (Show)
-type Env {-q-} = Map Var Value
-data Kont {-k-}
+type Machine {-m-} = (Counts,Control,Env,Kont)
+data Counts  {-i-} = Counts (Map Micro Int)
+data Control {-c-} = CE Exp | CV Value deriving (Show)
+type Env     {-q-} = Map Var Value
+data Kont    {-k-}
   = Kdone
   | Karg Env Exp Kont
   | Kfun Value Kont
@@ -45,30 +43,61 @@ data Kont {-k-}
   | Kbind Env Var Exp Kont
   deriving (Show)
 
+-- | take one step of the machine; the machine must not be in a finished state
 step :: Machine -> Machine
-step = \case
-  (CE (Num n), q, k) -> (CV (Number n), q, k)
-  (CE (Var x), q, k) -> (CV (look x q), q, k)
-  (CE (Lam x body), q, k) -> (CV (Clo q x body), q, k)
+step m = case tick (micsM m) m of
+  (i, CE (Num n), q, k)                 -> (i, CV (Number n), q, k)
+  (i, CE (Add e1 e2), q, k)             -> (i, CE e1, q, Kadd1 q e2 k)
+  (i, CE (Var x), q, k)                 -> (i, CV (look x q), q, k)
+  (i, CE (Lam x body), q, k)            -> tick [DoClose] (i, CV (Clo q x body), q, k)
+  (i, CE (App e1 e2), q, k)             -> (i, CE e1, q, Karg q e2 k)
+  (i, CE (Let x rhs body), q, k)        -> (i, CE rhs, q, Kbind q x body k)
 
-  (CV _, _, Kdone) -> error "stepping a finished machine"
-
-  (CE (App e1 e2), q, k) -> (CE e1, q, Karg q e2 k)
-  (CV v, _, Karg q e k) -> (CE e, q, Kfun v k)
-  (CV _, _, Kfun Number{} _) -> error "cant apply a non-function"
-  (CV v, _, Kfun (Clo q x e) k) ->  (CE e, Map.insert x v q, k)
-
-  (CE (Add e1 e2), q, k) -> (CE e1, q, Kadd1 q e2 k)
-  (CV v1, _, Kadd1 q e k) -> (CE e, q, Kadd2 v1 k)
-  (CV (Number n2), q, Kadd2 (Number n1) k) -> (CV (Number (n1+n2)), q, k)
-  (CV _, _, Kadd2 _ _) -> error "cant add non-numbers"
-
-  --(CE (Let x rhs body), q, k) -> (CE (App (Lam x body) rhs), q, k)
-  (CE (Let x rhs body), q, k) -> (CE rhs, q, Kbind q x body k)
-  (CV v, _, Kbind q x e k) -> (CE e, Map.insert x v q, k)
+  (_, CV _, _, Kdone)                   -> error "stepping a finished machine"
+  (i, CV v, _, Karg q e k)              -> (i, CE e, q, Kfun v k)
+  (_, CV _, _, Kfun Number{} _)         -> error "cant apply a non-function"
+  (i, CV v, _, Kfun (Clo q x e) k)      -> (i, CE e, Map.insert x v q, k)
+  (i, CV v1, _, Kadd1 q e k)            -> (i, CE e, q, Kadd2 v1 k)
+  (i, CV v1, q, Kadd2 v2 k)             -> tick [DoAddition] (i, CV (add v1 v2), q, k)
+  (i, CV v, _, Kbind q x e k)           -> (i, CE e, Map.insert x v q, k)
 
 
-data Counts = Counts (Map Class Int)
+look :: Var -> Env -> Value
+look x env = maybe (error $ "lookup:" ++ x) id (Map.lookup x env)
+
+add :: Value -> Value -> Value
+add (Number n1) (Number n2) = Number (n1+n2)
+add _ _ = error "can't add non-numbers"
+
+
+-- | The micro steps about to be taken, from a given machine state
+micsM :: Machine -> [Micro]
+micsM = \case
+  (_, CE e, _, _) -> [DoControlE, micsE e]
+  (_, CV _, _, k) -> [DoControlV, micsK k]
+
+micsE :: Exp -> Micro
+micsE = \case
+  Num{} -> DoNum
+  Add{} -> DoAdd
+  Var{} -> DoVar
+  Lam{} -> DoLam
+  App{} -> DoApp
+  Let{} -> DoLet
+
+micsK :: Kont -> Micro
+micsK = \case
+  Kdone{} -> DoKdone
+  Karg{} -> DoKarg
+  Kfun{} -> DoKfun
+  Kadd1{} -> DoKadd1
+  Kadd2{} -> DoKadd2
+  Kbind{} -> DoKbind
+
+
+-- | Record (in the machine state) a list of micro steps as having been taken
+tick :: [Micro] -> Machine -> Machine
+tick mics (i, c, q, k) = (foldl countMicro i mics, c, q, k)
 
 instance Show Counts where
   show (Counts m) =
@@ -77,45 +106,30 @@ instance Show Counts where
 counts0 :: Counts
 counts0 = Counts Map.empty
 
-bump :: Machine -> Counts -> Counts
-bump cek (Counts m) = Counts (Map.insertWith (+) (classify cek) 1 m)
+countMicro :: Counts -> Micro -> Counts
+countMicro (Counts mm) cl = Counts (Map.insertWith (+) cl 1 mm)
 
-data Class
-  -- At (CE exp)...
-  = AtNum
-  | AtAdd
-  | AtVar
-  | AtLam
-  | AtApp
-  | AtLet
-  -- At (CE value)...
-  | AtKdone
-  | AtKarg
-  | AtKfun
-  | AtKadd1
-  | AtKadd2
-  | AtKbind
+-- | Classification of _Micro_ steps taken by the machine
+data Micro
+  = DoOuterStep
+  | DoControlE
+  | DoControlV
+  -- Dispatch on control expression
+  | DoNum
+  | DoAdd
+  | DoVar
+  | DoLam
+  | DoApp
+  | DoLet
+  -- Dispatch on continuation (when control is a value)
+  | DoKdone
+  | DoKarg
+  | DoKfun
+  | DoKadd1
+  | DoKadd2
+  | DoKbind
+  -- Useful computation
+  | DoClose
+  | DoAddition
   deriving (Show,Eq,Ord)
 
-classify :: Machine -> Class
-classify = \case
-  (CE e, _, _) -> classifyE e
-  (CV _, _, k) -> classifyK k
-
-classifyE :: Exp -> Class
-classifyE = \case
-  Num{} -> AtNum
-  Add{} -> AtAdd
-  Var{} -> AtVar
-  Lam{} -> AtLam
-  App{} -> AtApp
-  Let{} -> AtLet
-
-classifyK :: Kont -> Class
-classifyK = \case
-  Kdone{} -> AtKdone
-  Karg{} -> AtKarg
-  Kfun{} -> AtKfun
-  Kadd1{} -> AtKadd1
-  Kadd2{} -> AtKadd2
-  Kbind{} -> AtKbind
